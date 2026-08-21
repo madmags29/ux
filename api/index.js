fb9b052ab3d49ce663dd8dfd3139984faea85c19
Majid Khan <mails365@gmail.com>
2026-08-21 13:29:13 +0530

    fix: route all /api/* requests via vercel.json to single api/index.js serverless function

--- api/index.js
+++ api/index.js
@@ -1,12 +1,3 @@
 import app from './server.js';
 
-export default async function handler(req, res) {
-  try {
-    return await app(req, res);
-  } catch (err) {
-    console.error('Vercel Serverless Error in api/index:', err);
-    if (!res.headersSent) {
-      res.status(500).json({ error: err.message, stack: err.stack });
-    }
-  }
-}
+export default app;
