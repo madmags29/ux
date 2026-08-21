2aedaaf7d35068ae7d7aaa119001ff9831f4190b
Majid Khan <mails365@gmail.com>
2026-08-21 13:22:06 +0530

    fix: export default app across api serverless handlers

--- api/evaluate.js
+++ api/evaluate.js
@@ -1,12 +1,3 @@
 import app from '../backend/server.js';
 
-export const config = {
-  maxDuration: 60,
-  api: {
-    bodyParser: false, // Allow multer to process multipart/form-data
-  },
-};
-
-export default function handler(req, res) {
-  return app(req, res);
-}
+export default app;
